468a4740f9aa23f60f84d4dd207b2fa4f8d9c00a
Latch <LatchGameDev@yahoo.com>
2025-12-09 16:45:27 -0600

    Code for tutorial "Item Groups | #4"
    No issues adding item groups following the video.

--- src/main/java/latch/tutorialmod/TutorialMod.java
+++ src/main/java/latch/tutorialmod/TutorialMod.java
@@ -1,6 +1,7 @@
 package latch.tutorialmod;
 
 import latch.tutorialmod.block.ModBlocks;
+import latch.tutorialmod.item.ModItemGroups;
 import latch.tutorialmod.item.ModItems;
 import net.fabricmc.api.ModInitializer;
 
@@ -13,6 +14,8 @@ public class TutorialMod implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
+		ModItemGroups.registerItemGroups();
+
 		ModItems.registerModItems();
 		ModBlocks.registerModBlocks();
 	}
